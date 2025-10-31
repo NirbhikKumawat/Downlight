f802addddc2e8d461896c30c684ff25599594f9c
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-10-31 21:31:31 +0530

    added preview pane in the editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
 import {ReadFile,WriteFile} from "../wailsjs/go/main/App";// import functions from Go backend
 
 function App() {
@@ -28,7 +29,12 @@ function App() {
                 <button className="btn" onClick={handleOpenFile}>Open File</button>
                 <button className="btn" onClick={handleSaveFile}>Save File</button>
             </div>
+            <div className="main-content">
             <textarea className="editor-pane" value={fileContent} onChange={(e)=> setFileContent(e.target.value)}/>{/*changing content in tet area*/}
+                <div className="preview-pane">
+                    <ReactMarkdown>{fileContent}</ReactMarkdown>
+                </div>
+            </div>
         </div>
     )
 }
